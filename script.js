6a2dc1df7ca644c4e46ff9735c1a21d88e88ab3b
mhamdNaser <115448369+mhamdNaser@users.noreply.github.com>
2022-12-14 17:04:02 +0300

    final day

--- script.js
+++ script.js
@@ -4,38 +4,59 @@ showname = () => {
 }
 
 
-    // Get a reference to the canvas element
-    var canvas = document.getElementById("myCanvas");
-    // Get the canvas 2D context, which is used to draw on the canvas
-    var ctx = canvas.getContext("2d");
+    // Create the canvas element
+    var canvas = document.createElement('canvas');
+    canvas.id = 'my-canvas';
 
-    // Set the starting position of the line
-    var x = 0;
-    var y = 200;
+    // Set the width and height of the canvas
+    canvas.width = 500;
+    canvas.height = 500;
 
-    // Set the line color and width
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 5;
+    // Add the canvas to the page
+    document.body.appendChild(canvas);
 
-    // Draw the line on the canvas
+    // Get a reference to the canvas context
+    var ctx = canvas.getContext('2d');
+
+    // Draw a ball on the canvas
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + 100, y);
-    ctx.stroke();
+    ctx.arc(250, 250, 50, 0, 2 * Math.PI);
+    ctx.fillStyle = 'red';
+    ctx.fill();
 
-    // Animate the line by moving it to the right
-    function animate() {
-        x += 5;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.lineTo(x + 100, y);
-        ctx.stroke();
-        requestAnimationFrame(animate);
-    }
 
-    // Start the animation
-    animate();
+    // // Get a reference to the canvas element
+    // var canvas = document.getElementById("myCanvas");
+    // // Get the canvas 2D context, which is used to draw on the canvas
+    // var ctx = canvas.getContext("2d");
+
+    // // Set the starting position of the line
+    // var x = 0;
+    // var y = 200;
+
+    // // Set the line color and width
+    // ctx.strokeStyle = "red";
+    // ctx.lineWidth = 5;
+
+    // // Draw the line on the canvas
+    // ctx.beginPath();
+    // ctx.moveTo(x, y);
+    // ctx.lineTo(x + 100, y);
+    // ctx.stroke();
+
+    // // Animate the line by moving it to the right
+    // function animate() {
+    //     x += 5;
+    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //     ctx.beginPath();
+    //     ctx.moveTo(x, y);
+    //     ctx.lineTo(x + 100, y);
+    //     ctx.stroke();
+    //     requestAnimationFrame(animate);
+    // }
+
+    // // Start the animation
+    // animate();
 
 
 //     // Get a reference to the video element on the page
